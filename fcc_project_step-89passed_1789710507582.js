0a16770a99959d123b9de7b3faf0dedc814a7c99
Kharade Vaishnavi Nilesh <vaishnavikharade263@gmail.com>
2026-09-18 11:18:29 +0530

    Sync FreeCodeCamp project

---
+++ fcc_project_step-89passed_1789710507582.js
@@ -0,0 +1,22 @@
+  background-color:  burlywood;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 20px;
+  max-width: 500px;
+}
+
+img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top:-25px;
+}
+
+hr {
+  height: 2px;
+  background-color:  brown;
+  border-color:  brown;
+}
+
+.bottom-line {
+  margin-top: 25px;
