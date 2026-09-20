5b8b6ebee6210aedd2e09cca9693a85d00a23f9f
Kharade Vaishnavi Nilesh <vaishnavikharade263@gmail.com>
2026-09-20 13:03:08 +0530

    Sync FreeCodeCamp project

---
+++ fcc_project_what-is-css-specificity-and-the-specific_1789889585457.js
@@ -0,0 +1,45 @@
+<link rel="stylesheet" href="styles.css">
+
+<p style="color: red;">Red paragraph</p>
+<p>Other paragraph</p>
+<p>Another paragraph</p>
+<p>Yet another paragraph</p>
+<link rel="stylesheet" href="styles.css">
+
+<p id="para-1">Red paragraph</p>
+<p>Other paragraph</p>
+<p>Another paragraph</p>
+<p>Yet another paragraph</p>
+<link rel="stylesheet" href="styles.css">
+
+<p id="para-1">Example paragraph</p>
+<p class="example-para">Other paragraph</p>
+<p id="para-3" class="example-para">Another paragraph</p>
+<p>Yet another paragraph</p>
+<link rel="stylesheet" href="styles.css">
+
+<p id="para-1">Example paragraph</p>
+<p>Other paragraph</p>
+<p>Yet another paragraph</p>
+<link rel="stylesheet" href="styles.css">
+
+<p id="para-1">Example paragraph</p>
+<p>Other paragraph</p>
+<p>Yet another paragraph</p>
+<p style="color: red;">This text is red.</p>
+<head>
+  <style>
+    #text {
+      color: blue;
+    }
+  </style>
+</head>
+<body>
+  <div id="text">This text is blue.</div>
+</body>
+<head>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <div class="text">This text's color is defined in an external CSS file.</div>
+</body>
